a09617409859c92645b1d82a964ab779088783b7
rajputlakshya619-source <rajputlakshya619@gmail.com>
2026-08-22 19:00:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ session_start();
     ?>
 
     <div class="banner-container">
-        <h2 class="white pb-3">Happiness per Square Foot</h2>
+        <h2 class="white banner-title">Happiness per Square Foot</h2>
 
         <form id="search-form" action="property_list.php" method="GET">
             <div class="input-group city-search">
